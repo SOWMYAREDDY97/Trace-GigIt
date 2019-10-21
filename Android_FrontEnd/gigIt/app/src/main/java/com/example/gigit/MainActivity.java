fbecb47284517393c0ad9bd9a5e7945d8ca46b5c
nikitha <nikithalakmarapu@gmail.com>
2019-10-20 19:17:38 -0500

    commited json parse code

--- Android_FrontEnd/gigIt/app/src/main/java/com/example/gigit/MainActivity.java
+++ Android_FrontEnd/gigIt/app/src/main/java/com/example/gigit/MainActivity.java
@@ -11,8 +11,13 @@ import okhttp3.Response;
 
 import android.os.Bundle;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
     private TextView textViewResult;
+    private String myResponse;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -32,16 +37,33 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call call, Response response) throws IOException {
                 if (response.isSuccessful()) {
                     textViewResult =findViewById(R.id.mTextViewResult);
-                    final String myResponse = response.body().string();
+                 myResponse = response.body().string();
                     MainActivity.this.runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
+                            //myResponse is the json data from the given url
                             textViewResult.setText(myResponse);
+                                    try {
+                                        //parsing data to get required value,here getting emails from the json data
+                                        JSONObject jObject = new JSONObject(myResponse);
+                                        JSONArray dataArray = jObject.getJSONArray("data");
+                                        String email ="";
+                                        for(int i=0;i<5;i++) {
+                                            JSONObject firstinput = dataArray.getJSONObject(i);
+
+                                                  email=  email+"\n"+firstinput.getString("email");
+                                        }
+                                        textViewResult.setText(email);
+                                    } catch (JSONException e) {
+                                        textViewResult.setText("error");
+                                    }
                         }
                     });
                 }
             }
         });
+
+
     }
 }
 

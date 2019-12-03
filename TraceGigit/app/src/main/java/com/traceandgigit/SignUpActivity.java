24d4e97161fe42cf0c0b7af18c1ed4c700387e12
DeepakJayanthi <46684265+DeepakJayanthi@users.noreply.github.com>
2019-12-02 22:08:32 -0600

    Modified sign-in activity.

--- TraceGigit/app/src/main/java/com/traceandgigit/SignUpActivity.java
+++ TraceGigit/app/src/main/java/com/traceandgigit/SignUpActivity.java
@@ -22,7 +22,6 @@ public class SignUpActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
         setContentView(R.layout.activity_signup);
 
         Button newUserButton = (Button) findViewById(R.id.newUserButton);

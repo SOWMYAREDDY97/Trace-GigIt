d9cfe746590f44ed11c43216f936f67e761873f2
Nandini Yadav Janga <yadavnandini97@gmail.com>
2020-02-19 09:18:07 -0600

    Added navigation view to employeeschedule details activity

--- TraceGigit/app/src/main/java/com/traceandgigit/Employee_ScheduleDetails.java
+++ TraceGigit/app/src/main/java/com/traceandgigit/Employee_ScheduleDetails.java
@@ -3,12 +3,17 @@ package com.traceandgigit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class Employee_ScheduleDetails extends AppCompatActivity {
+public class Employee_ScheduleDetails extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee__schedule_details);
+
+
     }
+
+
 }

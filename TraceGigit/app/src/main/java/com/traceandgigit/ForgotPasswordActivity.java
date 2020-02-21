09933af04da397fbcb199dc985cb831900238612
DeepakJayanthi <46684265+DeepakJayanthi@users.noreply.github.com>
2020-02-21 01:27:43 -0600

    adding forgot passowrd imports

--- TraceGigit/app/src/main/java/com/traceandgigit/ForgotPasswordActivity.java
+++ TraceGigit/app/src/main/java/com/traceandgigit/ForgotPasswordActivity.java
@@ -5,6 +5,18 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+// Parse Dependencies
+import com.parse.Parse;
+import com.parse.ParseUser;
+
 public class ForgotPasswordActivity extends Activity {
 
 
@@ -12,5 +24,6 @@ public class ForgotPasswordActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+        
     }
 }

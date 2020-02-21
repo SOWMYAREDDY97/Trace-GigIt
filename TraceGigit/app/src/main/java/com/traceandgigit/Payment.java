672e877ed02882fda96f8d517df452484a77b357
bharadwaj1995 <46728675+bharadwaj1995@users.noreply.github.com>
2020-02-21 13:26:15 -0600

    pushing working mapper

--- TraceGigit/app/src/main/java/com/traceandgigit/Payment.java
+++ TraceGigit/app/src/main/java/com/traceandgigit/Payment.java
@@ -1,4 +1,16 @@
 package com.traceandgigit;
 
-public class Payment {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Payment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.payment);
+
+
+    }
 }
--- TraceGigit/app/src/main/java/com/traceandgigit/saloonDetails.java
+++ TraceGigit/app/src/main/java/com/traceandgigit/saloonDetails.java
@@ -10,5 +10,9 @@ public class saloonDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saloon_details);
+
+
+
+
     }
 }

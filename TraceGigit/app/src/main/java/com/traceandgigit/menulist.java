87a5c22345874e2156f4a2451ab5340eb8104184
nikitha <nikithalakmarapu@gmail.com>
2020-02-23 18:06:50 -0600

    committed menulist files

---
+++ TraceGigit/app/src/main/java/com/traceandgigit/menulist.java
@@ -0,0 +1,31 @@
+package com.traceandgigit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class menulist extends AppCompatActivity {
+    private LinearLayout parentLinearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menulist);
+       parentLinearLayout = (LinearLayout) findViewById(R.id.parent_linear_layout);
+    }
+    public void onAddField(View v) {
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        //final View rowView = inflater.inflate(R.layout.field, null);
+        // Add the new row before the add field button.
+       // parentLinearLayout.addView(rowView, parentLinearLayout.getChildCount() - 1);
+    }
+
+    public void onDelete(View v) {
+        parentLinearLayout.removeView((View) v.getParent());
+    }
+
+}

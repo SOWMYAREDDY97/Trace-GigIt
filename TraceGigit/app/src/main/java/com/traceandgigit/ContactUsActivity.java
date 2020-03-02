9742a1ff8fd667c94ba6c97b6c1b919bd19e730b
bharadwaj1995 <46728675+bharadwaj1995@users.noreply.github.com>
2020-03-02 02:02:52 -0600

    send mail in contact us

--- TraceGigit/app/src/main/java/com/traceandgigit/ContactUsActivity.java
+++ TraceGigit/app/src/main/java/com/traceandgigit/ContactUsActivity.java
@@ -1,15 +1,78 @@
 package com.traceandgigit;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Hashtable;
+
+
 public class ContactUsActivity extends AppCompatActivity {
+
+    public EditText from , message;
+    public String to = "traceandgigit@gmail.com";
+    public Button send;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.help);
+
+        from = findViewById(R.id.user_email);
+        message = findViewById(R.id.message_body);
+        send = findViewById(R.id.sen_mail);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //OnClick event
+                Handler handler = new Handler(Looper.getMainLooper());
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        sendMailUsingSendGrid(from.getText().toString(), to,
+                                "Question from user",
+                                message.getText().toString());
+
+                        Toast.makeText(getApplicationContext(),"Sending mail...", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+
+    });
     }
+
+
+    private void sendMailUsingSendGrid(String from, String to, String subject, String mailBody){
+        Hashtable<String, String> params = new Hashtable<>();
+        params.put("to", to);
+        params.put("from", from);
+        params.put("subject", subject);
+        params.put("text", mailBody);
+
+        SendGridAsyncTask email = new SendGridAsyncTask();
+        try{
+            email.execute(params);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
 }
---
+++ TraceGigit/app/src/main/java/com/traceandgigit/SendGridCredentials.java
@@ -0,0 +1,14 @@
+package com.traceandgigit;
+
+public class SendGridCredentials {
+    public static final String username = "traceandgigit";
+    public static final String password = "R@m@r@m@123";
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}

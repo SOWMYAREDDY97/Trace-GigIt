8dc3df68dce0ccf58572658b83f3447258ed10af
SOWMYAREDDY97 <46725519+SOWMYAREDDY97@users.noreply.github.com>
2020-03-02 00:20:56 -0600

    bug fixed for forgot pswd

--- TraceGigit/app/src/main/java/com/traceandgigit/ForgotPasswordActivity.java
+++ TraceGigit/app/src/main/java/com/traceandgigit/ForgotPasswordActivity.java
@@ -5,18 +5,12 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
-import android.app.AlertDialog;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.Bundle;
 //import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 // Parse Dependencies
-import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.RequestPasswordResetCallback;
@@ -29,7 +23,7 @@ public class ForgotPasswordActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
-        email=findViewById(R.id.forgot_password_email);
+        email=findViewById(R.id.userId);
 
         
     }
